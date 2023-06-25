df6493e660e8a1655f23b9a0b164d8f306ce40ac
Aditya R Pathak <adityarpathak@Adityas-MacBook-Pro.local>
2023-06-25 16:37:07 +0530

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomeFilter from "./CustomeFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CardDetail from "./CardDetail";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomeFilter,
+  SearchManufacturer,
+  CarCard,
+  CardDetail,
+  ShowMore,
+};
